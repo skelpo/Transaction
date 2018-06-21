6adbbac58f25501829aff1725ad0c3a18e8cb384
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-21 08:04:03 -0500

    Installed vapor/vapor package

--- Package.swift
+++ Package.swift
@@ -8,9 +8,10 @@ let package = Package(
         .library(name: "Transaction", targets: ["Transaction"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
     ],
     targets: [
-        .target(name: "Transaction", dependencies: []),
-        .testTarget(name: "TransactionTests", dependencies: ["Transaction"]),
+        .target(name: "Transaction", dependencies: ["Vapor"]),
+        .testTarget(name: "TransactionTests", dependencies: ["Transaction", "Vapor"]),
     ]
 )
