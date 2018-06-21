4f3976106935bdfbc1af5e0a2d528327d44ed0fd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-21 12:20:16 -0500

    Added return type to paymentInit parameters in PaymentMethod methods

--- Sources/Transaction/PaymentMethod.swift
+++ Sources/Transaction/PaymentMethod.swift
@@ -32,11 +32,17 @@ public protocol PaymentMethod {
         userId: Int,
         amount: Int?,
         status: Purchase.PaymentStatus?,
-        paymentInit: (Purchase.ID, Int, Int, Purchase.PaymentStatus) -> ()
+        paymentInit: @escaping (Purchase.ID, String, Int, Int) -> (Purchase.Payment)
     ) -> Future<Purchase.Payment>
     
     /// Pays for a `Purchase` instance.
-    func pay(for order: Purchase, userId: Int, amount: Int, params: Codable?) throws -> Future<PaymentResponse<Purchase>>
+    func pay(
+        for order: Purchase,
+        userId: Int,
+        amount: Int,
+        params: Codable?,
+        paymentInit: @escaping (Purchase.ID, String, Int, Int) -> (Purchase.Payment)
+    ) throws -> Future<PaymentResponse<Purchase>>
     
     ///
     func refund(payment: Purchase.Payment, amount: Int?) -> Future<Purchase.Payment>
