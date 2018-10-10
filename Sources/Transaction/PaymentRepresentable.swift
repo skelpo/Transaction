731201e5bc0a9beac69e9c0f91f1231e0be0b37c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-10 12:56:05 -0500

    Added 'externalID' parameter to PaymentRepresentable.payment(on:with:) method

--- Sources/Transaction/PaymentRepresentable.swift
+++ Sources/Transaction/PaymentRepresentable.swift
@@ -4,12 +4,25 @@ import Service
 /// A type that can be converted to a payment provider's payment model.
 public protocol PaymentRepresentable {
     
+    ///
+    associatedtype ProviderPayment: Codable
+    
     /// The `Payment` type that `PaymentRepresentable` can be converted to.
     associatedtype Payment
     
     /// Converts `PaymentRepresentable` to the `Payment` type.
-    func payment<Method>(on container: Container, with method: Method) -> Future<Payment> where Method: PaymentMethod
+    func payment<Method, ID>(
+        on container: Container,
+        with method: Method,
+        externalID: ID
+    ) -> Future<Payment> where Method: PaymentMethod, ID: Identifiable
     
     /// Gets an already created `Payment` instance.
     func fetchPayment(on container: Container) -> Future<Payment>
+}
+
+public protocol Identifiable: Codable {
+    associatedtype ID: Codable
+    
+    var id: ID? { get }
 }
